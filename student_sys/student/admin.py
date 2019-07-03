b2c2becf6caa4722501763598777fa5f3884e50f
kzhang3002 <zkdupiao@126.com>
2019-07-03 23:20:48 +0800

    models

--- student_sys/student/admin.py
+++ student_sys/student/admin.py
@@ -1,30 +1,21 @@
-#! _*_ coding: utf-8 _*_
-from django.db import models
+from django.contrib import admin
+from .models import Student
 
 
-# Register your models here.
-class Student(models.Model):
-    SEX_ITEMS = {
-        (1, '男'),
-        (2, '女'),
-        (0, '未知'),
-    }
-    STATUS_ITEMS = [
-        (0, '申请'),
-        (1, '通过'),
-        (2, '拒绝'),
-    ]
-    name = models.CharField(max_length=128, verbose_name="姓名")
-    sex = models.IntegerField(choices=SEX_ITEMS, verbose_name="性别")
-    profession = models.CharField(max_length=128, verbose_name="职业")
-    email = models.EmailField(verbose_name="Email")
-    qq = models.CharField(max_length=128, verbose_name="QQ")
-    phone = models.CharField(max_length=128, verbose_name="电话")
-    status = models.IntegerField(choices=STATUS_ITEMS, default=0, verbose_name="审核状态")
-    created_time = models.DateTimeField(auto_now_add=True, editable=False, verbose_name="创建时间")
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'sex', 'profession', 'email', 'qq', 'phone', 'status', 'created_time')
+    list_filter = ('sex', 'status', 'created_time')
+    search_fields = ('name', 'profession')
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                ('sex', 'profession'),
+                ('email', 'qq', 'phone'),
+                'status',
+            )
+        }),
+    )
 
-    def __str__(self):
-        return '<Student: {}>'.format(self.name)
 
-    class Meta:
-        verbose_name = verbose_name_plural = "学员信息"
+admin.site.register(Student, StudentAdmin)
